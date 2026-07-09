5bb642083ae09363e8cec6a6c1abc1c3297a1f5e
ajayshankar176-star <ajayshankar176@gmail.com>
2026-07-09 08:49:37 +0530

    Update vpc.tf

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -28,7 +28,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1a"
+  availability_zone       = "us-east-1"
   map_public_ip_on_launch = true
 
   tags = {
@@ -42,7 +42,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-north-1b"
+  availability_zone = "us-east-1"
 
   tags = {
     Name = "private-subnet"
